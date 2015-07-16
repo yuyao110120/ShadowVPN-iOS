100c57a17598b5a2d24e74cba94f7b648fa85df6
Blankwonder <l@yach.me>
2015-07-16 16:11:51 +0800

    Initial Commit

---
+++ Tunnel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tunnel
+//
+//  Created by blankwonder on 7/16/15.
+//  Copyright © 2015 Yach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tunnel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tunnel
+//
+//  Created by blankwonder on 7/16/15.
+//  Copyright © 2015 Yach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
